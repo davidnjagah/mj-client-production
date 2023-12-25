f87a0dc4432ce0d2262be5af9bca7bca8438db7a
David Washi <dnjagah@gmail.com>
2023-12-25 15:33:11 +0300

    app.ts fix

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true }));
 
 
 app.use('/api/fetchimages', authMiddleware, handleImageGen);
